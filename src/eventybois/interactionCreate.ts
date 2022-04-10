40990d8bfffc89cc0a672a94e22a6a348759fbf8
thehairy <brokenempirebe@gmail.com>
2022-04-10 22:24:00 +0200

    fix: 🐛 fix

--- src/eventybois/interactionCreate.ts
+++ src/eventybois/interactionCreate.ts
@@ -1,14 +1,20 @@
 import Type from "@sapphire/type";
 import { ContextCommand, Event, ModalCommand, SlashCommand } from "../@types";
-import { clean } from "../util.js";
-import { AutocompleteInteraction, CommandInteraction, ContextMenuInteraction, MessageEmbed, ModalSubmitInteraction, Util } from "discord.js";
+import { clean, handleError } from "../util.js";
+import { AutocompleteInteraction, ChatInputCommandInteraction, ContextMenuCommandInteraction, EmbedBuilder, ModalSubmitInteraction, TextBasedChannel } from "discord.js";
 import type { Interaction } from 'discord.js';
 
 export const name: Event['name'] = 'interactionCreate';
 export const once: Event['once'] = false;
 
+/**
+ * Interaction event entry point
+ * 
+ * @param interaction Interaction
+ * @returns 
+ */
 export const cook: Event['cook'] = async (interaction: Interaction): Promise<void> => {
-    if (interaction.isCommand() || interaction.isContextMenu()) {
+    if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
         return void handleCommand(interaction);
     }
     if (interaction.isAutocomplete()) {
@@ -20,16 +26,21 @@ export const cook: Event['cook'] = async (interaction: Interaction): Promise<voi
     }
 };
 
-const handleCommand = async (interaction: CommandInteraction | ContextMenuInteraction): Promise<void> => {
+/**
+ * Handles a command interaction
+ * 
+ * @param interaction ChatInputcommandInteraction | ContextMenuCommandInteraction
+ */
+const handleCommand = async (interaction: ChatInputCommandInteraction | ContextMenuCommandInteraction): Promise<void> => {
     try {
-        if (interaction.isCommand()) {
+        if (interaction.isChatInputCommand()) {
             const command = interaction.client.commandbois.get(interaction.commandName) as SlashCommand | undefined;
             if (command) {
                 console.log(`Received command from ${interaction.user.tag} using ${interaction.commandName}!`);
                 await command.cook(interaction);
             }
         }
-        if (interaction.isContextMenu()) {
+        if (interaction.isContextMenuCommand()) {
             const command = interaction.client.commandbois.get(interaction.commandName) as ContextCommand | undefined;
             if (command) {
                 console.log(`Received command from ${interaction.user.tag} using ${interaction.commandName}!`);
@@ -37,24 +48,15 @@ const handleCommand = async (interaction: CommandInteraction | ContextMenuIntera
             }
         }
     } catch (err: any) {
-        console.log(err);
-        
-        if (!interaction.deferred && !interaction.replied)
-            await interaction.deferReply({ ephemeral: true });
-        
-        const evaluated = err.toString();
-        const type = new Type(err).toString();
-
-        const embed = new MessageEmbed()
-            .setTitle('An error occured')
-            .setDescription(`\`\`\`js\n${clean(evaluated.slice(0, 4096))}\n\`\`\``)
-            .addField('Type', `\`\`\`ts\n${type}\n\`\`\``)
-            .setFooter({ text: `${evaluated.length > 1 ? 'The error was longer than 4096 chars!' : ''}` });
-
-        interaction.editReply({ embeds: [embed] });
+        handleError(err, interaction.channel!);
     }
 };
 
+/**
+ * Handles an autocomplete interaction
+ * 
+ * @param interaction AutocompleteInteraction
+ */
 const handleAutocomplete = async (interaction: AutocompleteInteraction): Promise<void> => {
     try {
         const command = interaction.client.commandbois.get(interaction.commandName) as SlashCommand | undefined;
@@ -63,21 +65,16 @@ const handleAutocomplete = async (interaction: AutocompleteInteraction): Promise
             await command.cook(interaction);
         }
     } catch (err: any) {
-        console.warn(err);
-                
-        const evaluated = err.toString();
-        const type = new Type(err).toString();
-
-        const embed = new MessageEmbed()
-            .setTitle('An error occured')
-            .setDescription(`\`\`\`js\n${clean(evaluated.slice(0, 4096))}\n\`\`\``)
-            .addField('Type', `\`\`\`ts\n${type}\n\`\`\``)
-            .setFooter({ text: `${evaluated.length > 1 ? 'The error was longer than 4096 chars!' : ''}` });
-
-        interaction.channel?.send({ embeds: [embed] });
+        handleError(err, interaction.channel!);
     }
 }
 
+
+/**
+ * Handles a modal interaction
+ * 
+ * @param interaction ModalSubmitInteraction
+ */
 const handleModal = async (interaction: ModalSubmitInteraction): Promise<void> => {
     try {
         const modal = interaction.client.modalbois.get(interaction.customId) as ModalCommand | undefined;
@@ -86,17 +83,6 @@ const handleModal = async (interaction: ModalSubmitInteraction): Promise<void> =
             await modal.cook(interaction);
         }
     } catch (err: any) {
-        console.warn(err);
-                
-        const evaluated = err.toString();
-        const type = new Type(err).toString();
-
-        const embed = new MessageEmbed()
-            .setTitle('An error occured')
-            .setDescription(`\`\`\`js\n${clean(evaluated.slice(0, 4096))}\n\`\`\``)
-            .addField('Type', `\`\`\`ts\n${type}\n\`\`\``)
-            .setFooter({ text: `${evaluated.length > 1 ? 'The error was longer than 4096 chars!' : ''}` });
-
-        interaction.channel?.send({ embeds: [embed] });
+        handleError(err, interaction.channel!);
     }
 }
--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,5 @@
-import type { Presence } from "discord.js";
+import Type from "@sapphire/type";
+import { EmbedBuilder, Presence, TextBasedChannel } from "discord.js";
 
 export const clean = (text: string): string => {
     if (typeof (text) === 'string') {
@@ -24,4 +25,29 @@ export const replacePlaceholder = (text: string, aWith: string[]): string => {
         text = text.replace(/{}/, sWith);
     }
     return text;
+}
+
+/**
+ * Handles an error
+ * 
+ * @param error Error
+ * @param channel TextBasedChannel
+ */
+export const handleError = async (error: any, channel: TextBasedChannel): Promise<void> => {
+    try {
+        console.warn(error);
+                
+        const evaluated = error.toString();
+        const type = new Type(error).toString();
+
+        const embed = new EmbedBuilder()
+            .setTitle('An error occured')
+            .setDescription(`\`\`\`js\n${clean(evaluated.slice(0, 4096))}\n\`\`\``)
+            .addFields({ name: 'Type', value: `\`\`\`ts\n${type}\n\`\`\`` })
+            .setFooter({ text: `${evaluated.length > 1 ? 'The error was longer than 4096 chars!' : ''}` });
+
+        channel?.send({ embeds: [embed] });
+    } catch (err: any) {
+        console.warn(err);
+    }
 }
